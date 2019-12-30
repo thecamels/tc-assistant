fb27f6a11e33017d0389753dc934ef2e32c2bb94
Jakub Mikita <jakub@bracketspace.com>
2019-12-30 10:53:49 +0100

    hooks dump

--- inc/hooks.php
+++ inc/hooks.php
@@ -11,4 +11,3 @@
 add_action( 'admin_enqueue_scripts', array( $this->scripts, 'enqueue_scripts' ), 10, 1 );
 add_action( 'wp_ajax_tc_assistant_save_item', array( $this->checklist, '_ajax_save_item' ), 10, 0 );
 add_action( 'admin_menu', array( $this->core_screen, 'register_page' ), 10, 0 );
-add_action( 'admin_init', array( $this->core_screen, 'add_screen_notice' ), 10, 0 );
